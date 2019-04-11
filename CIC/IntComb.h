c27846784b5d37896dff90832437725cd88e6626
Bertold Van den Bergh <vandenbergh@bertold.org>
2019-04-11 23:36:53 +0200

    CIC Filter

---
+++ CIC/IntComb.h
@@ -0,0 +1,40 @@
+#ifndef FILTER_CIC_INTCOMB_H_
+#define FILTER_CIC_INTCOMB_H_
+
+namespace Filter
+{
+namespace CIC
+{
+
+template <typename T> class Comb
+{
+public:
+    inline T update(T in)
+    {
+        T out = in - value_;
+        value_ = in;
+        return out;
+    }
+private:
+    T value_ = 0;
+};
+
+template <typename T> class Integrator
+{
+public:
+    inline T update(T in)
+    {
+        T out = in + value_;
+        value_ = out;
+        return out;
+    }
+private:
+    T value_ = 0;
+};
+
+}
+}
+
+
+
+#endif /* FILTER_CIC_INTCOMB_H_ */
